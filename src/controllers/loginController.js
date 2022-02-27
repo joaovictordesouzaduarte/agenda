f7cbb644c218f0881b93a128b52c1a4755f7e0a7
jvduarte <victorduarte.ufrj@gmail.com>
2022-02-27 13:00:16 -0300

    adding login-logout routes

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,17 +1,52 @@
 const Login = require('../models/LoginModel')
 exports.index = (req, res) => {
+    if(req.session.user) return res.render('login-logado')
     res.render('login');
-}
+};
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
-    if(login.errors.length > 0){
-        req.flash('errors', login.errors);
-        req.session.save(()=> {
-            return res.redirect('/login');
-        });
-        return;
+    try{
+        const login = new Login(req.body);
+        await login.register();
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(()=> {
+                return res.redirect('/login');
+            });
+            return;
+        }    
+        req.flash('success', 'Seu usuário foi criado com sucesso!')
+        req.session.save(() => {
+            return res.redirect('/login')
+        });    
     }
-    res.send(login.errors);
+    catch(e){
+        res.render('404');
+        console.log(e);        
+    }
+};
+exports.login = async (req, res) => {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(()=> {
+                return res.redirect('/login');
+            });
+            return;
+        }    
+        req.flash('success', 'Usuário logado com sucesso!')
+        req.session.user = login.user;
+        req.session.save(() => {
+            return res.redirect('/login')
+        });    
+    }
+    catch(e){
+        res.render('404');
+        console.log(e);        
+    }
+};
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/')
 }
-
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -14,23 +15,48 @@ class Login {
         this.errors = [];
         this.user = null;
     }
+    async login() {
+        this.valida();
+        
+        if(this.errors.length > 0) return;        
+        this.user = await LoginModel.findOne({email: this.body.email});
+
+        if(!this.user) {
+            this.errors.push('Usuário não existe')
+            return;
+        }
+        
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha inválida')
+            return;
+        }        
+        
+    }
     async register() {
         this.valida();
         if(this.errors.length > 0) return;
-        try{
-            this.user = await LoginModel.create(this.body)
-        }
-        catch(e){
-            console.log(e)
-        }
+        
+        await this.userExist();
+        if(this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+        this.user = await LoginModel.create(this.body)
+        
     }   
+    async userExist(){
+        const user = await LoginModel.findOne({email: this.body.email});
+        if (user) this.errors.push('Usuário já existe');
+    }
     //Valida dados do usuário
     valida() {
         this.cleanUp();
         //Checa e-mail válido
         if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido')
         //Senha
-        if(this.body.password.length < 3 || this.body.password >= 50) this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        if(this.body.password.length > 50 || this.body.password.length < 3 ){
+            this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        }
     }
     cleanUp() {
         for(const key in this.body) {
