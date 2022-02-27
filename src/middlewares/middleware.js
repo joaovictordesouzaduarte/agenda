e055616859b3573dfb096aa860b27e3c67f1e1dd
jvduarte <victorduarte.ufrj@gmail.com>
2022-02-27 10:06:09 -0300

    adding login controller

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  res.locals.errors = req.flash('errors');
   next();
 };
 
@@ -11,6 +11,7 @@ exports.checkCsrfError = (err, req, res, next) => {
   if(err && 'EBADCSRFTOKEN' === err.code) {
     return res.render('404');
   }
+  next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
